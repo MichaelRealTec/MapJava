8d9b35a7ca33a0d542efedb3de401f37e15c5d46
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-05 20:12:29 -0300

    exemplo Map

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,12 @@ public class Program {
 		cookies.put("phone", "99711122");
 		
 		cookies.remove("email");
+		cookies.put("phone", "99771133"); // Ecreve por cima do primeiro phone 
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email")); // Quando o elemento não existe ele retorna nulo
+		System.out.println("Size: " + cookies.size()); // Quantos elementos tem
 		
 		System.out.println("ALL COOKIES:");
 		for (String key : cookies.keySet()) {
