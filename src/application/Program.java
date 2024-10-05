ed9a7f66d1ec165c6bbcd61b81dcacffb5b7e5ea
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-10-05 20:07:03 -0300

    Map<key, value>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,23 @@
 package application;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// Map - Java - DevSuperior
+		Map<String, String> cookies = new TreeMap<>();
+		cookies.put("username", "Maria"); // armazenando chave e valor
+		cookies.put("email", "maria@gmail.com");
+		cookies.put("phone", "99711122");
+		
+		cookies.remove("email");
+		
+		System.out.println("ALL COOKIES:");
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
 	}
 
 }
